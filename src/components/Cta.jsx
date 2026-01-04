56513ece4d8082581ec9ed90abdc9df06b1c5c5f
Sonia Parker <soniaparker1207@gmail.com>
2026-01-04 12:50:10 +1100

    btn variety in the product grid that are also in cart

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,15 +1,31 @@
 import add from "../assets/icons/icon-add-to-cart.svg";
 
-const Cta = ({ onClickHandle }) => {
+const Cta = ({ onClickHandle, type = "general", label }) => {
   return (
     <button
-      onClick={onClickHandle}
-      className="flex relative bg-rose-50 rounded-full font-semibold text-rose-900 text-xs items-center gap-0.5 px-2 py-1.5 justify-center border border-rose-500 cursor-pointer hover:text-primary-red transition hover:border-primary-red w-3/4 bottom-5 left-1/2 -translate-x-1/2 z-50"
+      onClick={() => type === "general" && onClickHandle()}
+      className={`${
+        type === "tools"
+          ? "bg-primary-red border-primary-red text-rose-50 justify-between"
+          : "justify-center border-rose-500 cursor-pointer transition hover:border-primary-red bg-rose-50 text-rose-500 hover:text-primary-red "
+      } flex relative rounded-full font-semibold text-xs items-center gap-0.5 px-2 py-1.5 border w-3/4 bottom-5 left-1/2 -translate-x-1/2 z-50`}
     >
-      <span>
-        <img className="w-5" src={add} />
-      </span>
-      Add to Cart
+      {type === "general" && (
+        <>
+          <span>
+            <img className="w-5" src={add} />
+          </span>{" "}
+          <span>{label}</span>{" "}
+        </>
+      )}
+
+      {type === "tools" && (
+        <>
+          <span className="">-</span>
+          <span>{label}</span>
+          <span className="cursor-pointer" onClick={onClickHandle}>+</span>
+        </>
+      )}
     </button>
   );
 };
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,16 +3,24 @@ import { useCart } from "../context/CartContext";
 
 const ProductCard = ({ product }) => {
   
-  const {addToCart} = useCart();
+  const {cart, addToCart} = useCart();
+  const exists = cart.some(cartItem => cartItem.id === product.id);
+  const objSelected = cart.find((item) => item.id === product.id);
 
   return (
     <div>
       <div className="relative">
         <img
           src={product.image.desktop}
-          className="rounded-lg aspect-square w-full max-w-md object-cover"
+          className=
+          {`rounded-lg aspect-square w-full max-w-md object-cover ${exists ? 'border-primary-red border-2' : ''}`}
         />
-        <Cta onClickHandle={()=>addToCart(product)}/>
+        {exists ? (
+          <Cta label={objSelected.qty} type="tools" onClickHandle={()=>addToCart(product)}/>
+        ) : (
+        <Cta onClickHandle={()=>addToCart(product)} label="Add to Cart"/>
+
+        )}
       </div>
       <p className="text-rose-400 text-xs font-semibold">{product.category}</p>
       <h3 className="text-base font-semibold text-rose-900">{product.name}</h3>
